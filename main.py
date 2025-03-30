f125998ac5eb78629fc7b5696265a09b825bfd1e
Marc Altmann <marcaltmann@posteo.de>
2025-03-30 22:38:50 +0200

    Try out mt-940 parser

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import mt940
+import pprint
+
+
 def main():
-    print("Hello from wodeqian!")
+    transactions = mt940.parse('umsaetze.sta')
+
+    print('Transactions:')
+    print(transactions)
+    pprint.pprint(transactions.data)
+
+    print()
+    for transaction in transactions:
+        print('Transaction: ', transaction)
+        breakpoint()
+        pprint.pprint(transaction.data)
+        pprint.pprint(transaction.data["amount"])
+
 
 
 if __name__ == "__main__":
