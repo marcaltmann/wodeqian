d288d925acac5fdbb819febbcb0b9a62bc20bed6
Marc Altmann <marcaltmann@posteo.de>
2025-03-30 23:25:33 +0200

    Convert to CSV

--- main.py
+++ main.py
@@ -1,21 +1,41 @@
-import mt940
+import csv
 import pprint
+
+import mt940
+from mt940.models import Transaction
+
+
+def convert_transaction(transaction: Transaction) -> list:
+    return {
+        "date": transaction.data["date"],
+        "amount": transaction.data["amount"].amount,
+        "applicant": transaction.data["applicant_name"],
+        "purpose": transaction.data["purpose"],
+    }
 
 
 def main():
-    transactions = mt940.parse('umsaetze.sta')
+    transactions = mt940.parse("/home/marc/umsaetze.sta")
 
-    print('Transactions:')
-    print(transactions)
-    pprint.pprint(transactions.data)
-
-    print()
     for transaction in transactions:
-        print('Transaction: ', transaction)
-        breakpoint()
-        pprint.pprint(transaction.data)
-        pprint.pprint(transaction.data["amount"])
+        pprint.pprint(convert_transaction(transaction))
 
+    with open("transactions.csv", "w", newline="") as csvfile:
+        spamwriter = csv.writer(
+            csvfile, delimiter=",", quotechar="|", quoting=csv.QUOTE_MINIMAL
+        )
+        spamwriter.writerow(["Date", "Amount", "Applicant", "Purpose"])
+
+        for transaction in transactions:
+            converted = convert_transaction(transaction)
+            spamwriter.writerow(
+                [
+                    converted["date"],
+                    converted["amount"],
+                    converted["applicant"],
+                    converted["purpose"],
+                ]
+            )
 
 
 if __name__ == "__main__":
