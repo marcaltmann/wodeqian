368ee9c927219d02fc57bcee141858ac33104fe5
Marc Altmann <marcaltmann@posteo.de>
2025-04-18 19:29:46 +0200

    Add JSON conversion

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import csv
+import datetime
+import json
 import pprint
 
 import mt940
@@ -8,7 +10,7 @@ from mt940.models import Transaction
 ACCOUNT_TYPES = ["Assets", "Liabilities", "Equity", "Income", "Expenses"]
 
 
-def convert_transaction(transaction: Transaction) -> list:
+def convert_transaction(transaction: Transaction) -> dict:
     return {
         "date": transaction.data["date"],
         "amount": transaction.data["amount"].amount,
@@ -16,6 +18,13 @@ def convert_transaction(transaction: Transaction) -> list:
         "purpose": transaction.data["purpose"],
     }
 
+def prepare_for_json(transaction: dict) -> dict:
+    return {
+        "date": str(transaction["date"]),
+        "amount": float(transaction["amount"]),
+        "applicant": transaction["applicant"],
+        "purpose": transaction["purpose"],
+    }
 
 def format_transaction(transaction: list) -> str:
     date = transaction["date"].strftime("%Y-%m-%d")
@@ -31,6 +40,9 @@ def main():
             line = format_transaction(convert_transaction(transaction))
             f.write(line)
 
+    with open("transactions.json", "w") as f:
+        converted_transactions = [prepare_for_json(convert_transaction(t)) for t in transactions]
+        json.dump(converted_transactions, f, indent=4)
 
 if __name__ == "__main__":
     main()
