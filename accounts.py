792fd2abae698b3616718b67d02807f811b44a6b
Marc Altmann <marcaltmann@posteo.de>
2025-04-21 12:13:18 +0200

    Work on accounts

--- accounts.py
+++ accounts.py
@@ -1,2 +1,21 @@
+from dataclasses import dataclass
+from enum import StrEnum
+
+
+class AccountType(StrEnum):
+    """Represents the five account types."""
+    ASSETS = "Assets"
+    LIABILITIES = "Liabilities"
+    EQUITY = "Equity"
+    INCOME = "Income"
+    EXPENSES = "Expenses"
+
+
+@dataclass
 class Account:
-    ACCOUNT_TYPES = ["Assets", "Liabilities", "Equity", "Income", "Expenses"]
+    """Represents accounts."""
+    type: AccountType
+    name: str
+
+    def __str__(self):
+        return f"{self.type}:{self.name}"
