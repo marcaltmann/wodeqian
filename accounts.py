587a264840f7e5c03953fb8397aa19ee484667d3
Marc Altmann <marcaltmann@posteo.de>
2025-04-21 04:49:49 +0200

    Start work on Account class

---
+++ accounts.py
@@ -0,0 +1,2 @@
+class Account:
+    ACCOUNT_TYPES = ["Assets", "Liabilities", "Equity", "Income", "Expenses"]
--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import mt940
 from mt940.models import Transaction as Mt940Transaction
 
 from transactions import Transaction
-
-ACCOUNT_TYPES = ["Assets", "Liabilities", "Equity", "Income", "Expenses"]
-
+from accounts import Account
 
 def main():
     transactions = mt940.parse("transactions.sta")
+
+    print(Account.ACCOUNT_TYPES)
 
     with open("transactions.bean", "w", newline="") as f:
         for transaction in transactions:
