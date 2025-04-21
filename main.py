d1718d0d21629dc96ce32e587984ad7d4686c430
Marc Altmann <marcaltmann@posteo.de>
2025-04-21 04:11:11 +0200

    Refactor

--- main.py
+++ main.py
@@ -1,37 +1,20 @@
-import csv
-import datetime
 import json
-import pprint
 
 import mt940
-from mt940.models import Transaction
+from mt940.models import Transaction as Mt940Transaction
 
+from transactions import Transaction
 
 ACCOUNT_TYPES = ["Assets", "Liabilities", "Equity", "Income", "Expenses"]
 
 
-def convert_transaction(transaction: Transaction) -> dict:
-    return {
-        "date": transaction.data["date"],
-        "amount": transaction.data["amount"].amount,
-        "applicant": transaction.data["applicant_name"],
-        "purpose": transaction.data["purpose"],
-    }
-
-
-def prepare_for_json(transaction: dict) -> dict:
-    return {
-        "date": str(transaction["date"]),
-        "amount": float(transaction["amount"]),
-        "applicant": transaction["applicant"],
-        "purpose": transaction["purpose"],
-    }
-
-
-def format_transaction(transaction: list) -> str:
-    date = transaction["date"].strftime("%Y-%m-%d")
-    purpose = f"{transaction['applicant']}: {transaction['purpose']}"
-    return f'{date} * "{purpose}"\n\n'
+def convert_transaction(transaction: Mt940Transaction) -> Transaction:
+    return Transaction(
+        date=transaction.data["date"],
+        amount=transaction.data["amount"].amount,
+        applicant=transaction.data["applicant_name"],
+        purpose=transaction.data["purpose"],
+    )
 
 
 def main():
@@ -39,12 +22,12 @@ def main():
 
     with open("transactions.bean", "w", newline="") as f:
         for transaction in transactions:
-            line = format_transaction(convert_transaction(transaction))
+            line = convert_transaction(transaction).format_bean()
             f.write(line)
 
     with open("transactions.json", "w") as f:
         converted_transactions = [
-            prepare_for_json(convert_transaction(t)) for t in transactions
+            convert_transaction(t).as_json_dict() for t in transactions
         ]
         json.dump(converted_transactions, f, indent=4)
 
--- transactions.py
+++ transactions.py
@@ -14,3 +14,15 @@ class Transaction:
 
     def __str__(self) -> str:
         return f"{self.date} {self.amount}€ {self.applicant} {self.purpose}"
+
+    def as_json_dict(self) -> dict:
+        return {
+            "date": str(self.date),
+            "amount": float(self.amount),
+            "applicant": self.applicant,
+            "purpose": self.purpose,
+        }
+
+    def format_bean(self) -> str:
+        date_str = self.date.strftime("%Y-%m-%d")
+        return f'{date_str} * "{self.applicant}" "{self.purpose}"\n\n'
